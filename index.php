a2866a12d5288ec54ee3ac427438e81402d658ee
AnasLAM1 <202170971+AnasLAM1@users.noreply.github.com>
2025-08-18 01:41:53 +0000

    Practice

--- index.php
+++ index.php
@@ -3,8 +3,73 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Demo</title>
+    <title>Document</title>
 </head>
 <body>
+    <?php
+        $foods = [
+            [
+                'name' => 'Shakshouka',
+                'country' => 'Algeria',
+                'occasion' => 'Breakfast',
+                'continent' => 'Africa',
+            ],
+            [
+                'name' => 'Shawarma',
+                'country' => 'Syria',
+                'occasion' => 'Lunch',
+                'continent' => 'Asia',
+            ],
+            [
+                'name' => 'Kabsa',
+                'country' => 'Saudi Arabia',
+                'occasion' => 'Dinner',
+                'continent' => 'Asia',
+            ],
+            [
+                'name' => 'Magalgal',
+                'country' => 'Yemen',
+                'occasion' => 'Lunch',
+                'continent' => 'Asia',
+            ],
+            [
+                'name' => 'Menemen',
+                'country' => 'Turkey',
+                'occasion' => 'Breakfast',
+                'continent' => 'Asia',
+            ],
+            [
+                'name' => 'Sushi',
+                'country' => 'Japan',
+                'occasion' => 'Lunch',
+                'continent' => 'Asia',
+            ],
+            [
+                'name' => 'Chicken Katsu',
+                'country' => 'Japan',
+                'occasion' => 'Dinner',
+                'continent' => 'Asia'
+            ],
+        ];
+
+    function filterByContinent($foods) {
+        $filteredFoods = [];
+    
+    foreach($foods as $food) {
+        if ($food['continent'] === 'Asia') {
+        $filteredFoods[] = $food;
+        }
+    }
+
+    return $filteredFoods;
+
+    }
+
+?>
+<ul>
+    <?php foreach (filterByContinent($foods) as $food) : ?>
+        <li><?= $food['name'] ?></li>
+    <?php endforeach; ?>
+</ul>
 </body>
 </html>
---
+++ s.php
@@ -0,0 +1,56 @@
+<!-- Functions & Filters Practice-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+
+<body>
+    <?php
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'http://example.com'
+            ]
+        ];
+
+        function filterByAuthor($books) {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === 'Andy Weir') {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+
+        } 
+    ?>
+
+    <ul>
+        <?php foreach (filterByAuthor($books) as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) By <?= $book['author'] ?> 
+                </a>
+            </li>  
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
